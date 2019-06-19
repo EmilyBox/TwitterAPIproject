76c281634728a30c77919fbd63865559edd75ca3
EmilyBox <eeb1795@gmail.com>
2019-06-19 18:38:43 -0500

    ...

---
+++ modules/authConfig.js
@@ -0,0 +1,7 @@
+"user strict";
+
+module.exports = {
+    //TODO: Supply values for your app
+    TWITTER_CONSUMER_KEY: 'enter twitter consumer key here',
+    TWITTER_CONSUMER_SECRET: 'enter twitter consumer secret here'
+}
